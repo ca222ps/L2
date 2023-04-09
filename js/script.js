3fab74f002b1ef8e26d81c963a7e34e2f83e5d3a
Claudia Gutenberg <ca222ps@student.lnu.se>
2023-04-09 20:20:13 +0200

    1

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@
 // --------------------------------------------------
 // Initiering av globala variabler och händelsehanterare
 function init() {
+    inp1Elem = document.getElementById("input1");
+    inp2Elem = document.getElementById("input2");
+    msgElem = document.getElementById("message");
 	
 } // Slut init
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
