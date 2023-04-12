9a1de22829b79fbff3f71cf4e9238200c3417a73
Claudia Gutenberg <ca222ps@student.lnu.se>
2023-04-12 17:38:01 +0200

    3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Globala variabler
-var inp1Elem;
-var inp2Elem;
-var msgElem;
+var inp1Elem;       // Referens till textfält för input
+var inp2Elem;       // Referens till textfält för input
+var msgElem;        // Referens till div
 
 // --------------------------------------------------
 // Initiering av globala variabler och händelsehanterare
@@ -9,8 +9,22 @@ function init() {
     inp1Elem = document.getElementById("input1");
     inp2Elem = document.getElementById("input2");
     msgElem = document.getElementById("message");
+    document.getElementById("btn1").onclick = showFruit;
 	
 } // Slut init
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 // --------------------------------------------------
 
+function showFruit () {
+    let nr = Number(inp1Elem.value); // För att avläsa fruktens tal från input 1
+    let url; // Bild url
+    switch (nr) {
+        case 1: url = "img/apple.png"; break;
+        case 2: url = "img/banana.png"; break;
+        case 3: url = "img/pineapple.png"; break;
+        case 4: url = "img/orange.png"; break;
+        case 5: url = "img/pear.png"; break;
+        default: url= "img/nofruit.png";
+    }
+    document.getElementById("fruitImg").src = url;
+} // Slut showFruit
